6437e2576e44417df016dc3a731ea7cb8046f3ae
Emil Berzen <emilberzen@gmail.com>
2020-04-08 13:02:54 +0200

    files

---
+++ Assets/Scripts/Planning/CameraMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    [SerializeField] private Camera cam;
+
+    private Vector3 previousPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            previousPos = cam.ScreenToViewportPoint(Input.mousePosition);
+
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+
+            Vector3 direction = previousPos - cam.ScreenToViewportPoint(Input.mousePosition);
+
+            cam.transform.position = new Vector3();
+            cam.transform.Rotate(new Vector3(1, 0, 0), direction.y * 10);
+            cam.transform.Rotate(new Vector3(0, 1, 0), -direction.x * 10, Space.World);
+            cam.transform.Translate(new Vector3(0, 0, -3));
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/Planning/ClickAction.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAction : MonoBehaviour
+{
+
+    public GameObject UI;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CastRay();
+        }
+    }
+
+    void CastRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            Debug.Log("Hit object: " + hit.collider.gameObject.name);
+
+            if (hit.collider.gameObject.name == "Sweden")
+            {
+                Debug.Log("You hit the Sweden Telescope");
+                UI.SetActive(true);
+            }
+            else if (hit.collider.gameObject.name == "London")
+            {
+                UI.SetActive(true);
+                Debug.Log("You hit the London Telescope");
+
+            }
+            else if (hit.collider.gameObject.name == "Florida")
+            {
+                UI.SetActive(true);
+                Debug.Log("You hit the Florida Telescope");
+
+            }
+            else if (hit.collider.gameObject.name == "Barcelona")
+            {
+                UI.SetActive(true);
+                Debug.Log("You hit the Barca Telescope");
+
+            }
+            else if (hit.collider.gameObject.name == "Rome")
+            {
+                UI.SetActive(true);
+                Debug.Log("You hit the Rome Telescope");
+
+            }
+            else if (hit.collider.gameObject.name == "LA")
+            {
+                UI.SetActive(true);
+                Debug.Log("You hit the LAq Telescope");
+
+            }
+            else if(hit.collider.gameObject.name == "Sphere128")
+            {
+                UI.SetActive(false);
+            }
+        }
+    }
+}
