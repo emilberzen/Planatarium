650c3594ac2246ba9d2b9549f3ada40f0dad262d
Emil Berzén <emilberzen@gmail.com>
2020-04-20 23:03:49 +0200

    Updated project
    -New location
    -Text and image Info
    -Rotations

--- Assets/Scripts/Planning/CameraMove.cs
+++ Assets/Scripts/Planning/CameraMove.cs
@@ -6,30 +6,60 @@ public class CameraMove : MonoBehaviour
 {
 
     [SerializeField] private Camera cam;
+    public GameObject Target;
+    public GameObject Earth;
 
-    private Vector3 previousPos;
+    public float speed = 1f;
+    public float sensitivity = 1f;
+    public float minFov = 5f;
+    public float maxFov =50f;
+
+    public float speedNear = 0.5f;
+    public float speedFar = 5; 
+
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            previousPos = cam.ScreenToViewportPoint(Input.mousePosition);
+  
 
-        }
-
+        //Rotation
         if (Input.GetMouseButton(0))
         {
 
-            Vector3 direction = previousPos - cam.ScreenToViewportPoint(Input.mousePosition);
 
-            cam.transform.position = new Vector3();
-            cam.transform.Rotate(new Vector3(1, 0, 0), direction.y * 10);
-            cam.transform.Rotate(new Vector3(0, 1, 0), -direction.x * 10, Space.World);
-            cam.transform.Translate(new Vector3(0, 0, -3));
+            Target.transform.position = new Vector3();
+            Target.transform.Rotate(new Vector3(1, 0, 0), Input.GetAxis("Mouse Y") * -speed);
+            Target.transform.Rotate(new Vector3(0, 1, 0), Input.GetAxis("Mouse X") * speed, Space.World);
+            Target.transform.Translate(new Vector3(0, 0, -3));
+
+
+        }
+        else
+        {
+            //Rotates the sun slowly 
+            //Earth.transform.Rotate(new Vector3(0, 0.001f, 0));
         }
 
+        //ZOOM
+        float fov = Camera.main.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * -sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+
+        if (fov < 20)
+        {
+
+            speed = speedNear;
+        }
+        else
+        {
+            speed = speedFar;
+        }
 
     }
+
+
 }
+
